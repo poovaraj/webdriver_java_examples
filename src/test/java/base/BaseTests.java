7bde60e89a876891b849585e0d134ee0dd305acc
poovaraj <poovaraj2016@gmail.com>
2020-02-07 21:26:58 +0530

    Added WebDriver

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,6 +1,9 @@
 package base;
 
 import com.google.common.io.Files;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,54 +20,55 @@ import java.io.IOException;
 
 public class BaseTests {
 
-    private EventFiringWebDriver driver;
-    protected HomePage homePage;
+	private EventFiringWebDriver driver;
+	protected HomePage homePage;
 
-    @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
-        driver.register(new EventReporter());
-        goHome();
-    }
+	@BeforeClass
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		//driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+		driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
+		driver.register(new EventReporter());
+		goHome();
+	}
 
-    @BeforeMethod
-    public void goHome(){
-        driver.get("https://the-internet.herokuapp.com/");
-        homePage = new HomePage(driver);
-    }
+	@BeforeMethod
+	public void goHome() {
+		driver.get("https://the-internet.herokuapp.com/");
+		homePage = new HomePage(driver);
+	}
 
-    @AfterClass
-    public void tearDown(){
-        driver.quit();
-    }
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 
-    @AfterMethod
-    public void recordFailure(ITestResult result){
-        if(ITestResult.FAILURE == result.getStatus())
-        {
-            var camera = (TakesScreenshot)driver;
-            File screenshot = camera.getScreenshotAs(OutputType.FILE);
-            try{
-                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-        }
-    }
+	@AfterMethod
+	public void recordFailure(ITestResult result) {
+		if (ITestResult.FAILURE == result.getStatus()) {
+			var camera = (TakesScreenshot) driver;
+			File screenshot = camera.getScreenshotAs(OutputType.FILE);
+			try {
+				Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    public WindowManager getWindowManager(){
-        return new WindowManager(driver);
-    }
+	public WindowManager getWindowManager() {
+		return new WindowManager(driver);
+	}
 
-    private ChromeOptions getChromeOptions(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-infobars");
-        //options.setHeadless(true);
-        return options;
-    }
+	private ChromeOptions getChromeOptions() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disable-infobars");
+		// options.setHeadless(true);
+		return options;
+	}
 
-    public CookieManager getCookieManager(){
-        return new CookieManager(driver);
-    }
+	public CookieManager getCookieManager() {
+		return new CookieManager(driver);
+	}
 }
